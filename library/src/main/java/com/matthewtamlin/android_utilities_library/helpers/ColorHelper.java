ac938c6e2d206d8c0e076bafc765e7a03a183ad9
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-07-26 17:34:57 +0930

    Updated ColorHelper.java
    - Removed methods for accessing theme colors (now in a separate class)
    - Javadoc updates

--- library/src/main/java/com/matthewtamlin/android_utilities_library/helpers/ColorHelper.java
+++ library/src/main/java/com/matthewtamlin/android_utilities_library/helpers/ColorHelper.java
@@ -16,12 +16,7 @@
 
 package com.matthewtamlin.android_utilities_library.helpers;
 
-import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.util.TypedValue;
-
-import com.matthewtamlin.android_utilities_library.R;
 
 /**
  * Helper class for working with colors.
@@ -30,7 +25,8 @@ public abstract class ColorHelper {
 	/**
 	 * Blends two colours to produce a single output colour. No check is done to ensure the provided
 	 * colors are valid ARGB hex codes, and providing invalid codes will result in an undefined
-	 * result.
+	 * result. The blend works individually combining the ARGB components of the supplied colors and
+	 * then synthesising the components into one color.
 	 *
 	 * @param color1
 	 * 		the first colour to blend, as an ARGB hex code
@@ -58,89 +54,5 @@ public abstract class ColorHelper {
 
 		// Compose the result from the combined ARGB components
 		return Color.argb((int) a, (int) r, (int) g, (int) b);
-	}
-
-	/**
-	 * Extracts the primary color from the current theme of a Context.
-	 *
-	 * @param context
-	 * 		the Context containing the theme to query, not null
-	 * @param defaultColor
-	 * 		the color to return if no primary color is found, as an ARGB hex code
-	 * @return the primary color, or the default color if none is found
-	 * @throws IllegalArgumentException
-	 * 		if {@code context} is null
-	 */
-	public static int getPrimaryColor(final Context context, final int defaultColor) {
-		if (context == null) {
-			throw new IllegalArgumentException("context cannot be null");
-		}
-
-		return getColor(context, defaultColor, R.attr.colorPrimary);
-	}
-
-	/**
-	 * Extracts the primary dark color from the current theme of a Context.
-	 *
-	 * @param context
-	 * 		the Context containing the theme to query, not null
-	 * @param defaultColor
-	 * 		the color to return if no primary dark color is found, as an ARGB hex code
-	 * @return the primary dark color, or the default color if none is found
-	 * @throws IllegalArgumentException
-	 * 		if {@code context} is null
-	 */
-	public static int getPrimaryDarkColor(final Context context, final int defaultColor) {
-		if (context == null) {
-			throw new IllegalArgumentException("context cannot be null");
-		}
-
-		return getColor(context, defaultColor, R.attr.colorPrimaryDark);
-	}
-
-	/**
-	 * Extracts the accent color from the current theme of a Context.
-	 *
-	 * @param context
-	 * 		the Context containing the theme to query, not null
-	 * @param defaultColor
-	 * 		the color to return if no accent color is found, as an ARGB hex code
-	 * @return the accent color, or the default color if none is found
-	 * @throws IllegalArgumentException
-	 * 		if {@code context} is null
-	 */
-	public static int getAccentColor(final Context context, final int defaultColor) {
-		if (context == null) {
-			throw new IllegalArgumentException("context cannot be null");
-		}
-
-		return getColor(context, defaultColor, R.attr.colorAccent);
-	}
-
-	/**
-	 * Extracts a color from the current theme of a Context.
-	 *
-	 * @param context
-	 * 		the Context containing the theme to query, not null
-	 * @param defaultColor
-	 * 		the color to return if no color is found, as an ARGB hex code
-	 * @param colorAttr
-	 * 		an attribute in the current theme which specifies the color to return
-	 * @return the specified color, or the default color if none is found
-	 * @throws IllegalArgumentException
-	 * 		if {@code context} is null
-	 */
-	private static int getColor(final Context context, final int defaultColor, final int
-			colorAttr) {
-		if (context == null) {
-			throw new IllegalArgumentException("context cannot be null");
-		}
-
-		final TypedValue v = new TypedValue();
-		final TypedArray a = context.obtainStyledAttributes(v.data, new int[]{colorAttr});
-		final int color = a.getColor(0, defaultColor);
-
-		a.recycle();
-		return color;
 	}
 }
