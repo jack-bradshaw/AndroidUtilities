5722f63f7b7d83c96bee96a6913882fc917d9a2f
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-07-26 20:08:52 +0930

    Updated ThemeColorHelper.java
    - Updated Javadoc
    - Added copyright notice

--- library/src/main/java/com/matthewtamlin/android_utilities_library/helpers/ThemeColorHelper.java
+++ library/src/main/java/com/matthewtamlin/android_utilities_library/helpers/ThemeColorHelper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Matthew Tamlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.matthewtamlin.android_utilities_library.helpers;
 
 import android.content.Context;
@@ -7,11 +23,11 @@ import android.util.TypedValue;
 import com.matthewtamlin.android_utilities_library.R;
 
 /**
- * Created by Matthew on 2016.07.23.
+ * Utilities for extracting the primary, primary dark and accent colors from a theme.
  */
 public class ThemeColorHelper {
 	/**
-	 * Extracts the primary color from the current theme of a Context.
+	 * Extracts the primary color from the current theme of the supplied Context.
 	 *
 	 * @param context
 	 * 		the Context containing the theme to query, not null
@@ -30,7 +46,7 @@ public class ThemeColorHelper {
 	}
 
 	/**
-	 * Extracts the primary dark color from the current theme of a Context.
+	 * Extracts the primary dark color from the current theme of the supplied Context.
 	 *
 	 * @param context
 	 * 		the Context containing the theme to query, not null
@@ -49,7 +65,7 @@ public class ThemeColorHelper {
 	}
 
 	/**
-	 * Extracts the accent color from the current theme of a Context.
+	 * Extracts the accent color from the current theme of the supplied Context.
 	 *
 	 * @param context
 	 * 		the Context containing the theme to query, not null
@@ -68,7 +84,7 @@ public class ThemeColorHelper {
 	}
 
 	/**
-	 * Extracts a color from the current theme of a Context.
+	 * Extracts a color from the current theme of the supplied Context.
 	 *
 	 * @param context
 	 * 		the Context containing the theme to query, not null
