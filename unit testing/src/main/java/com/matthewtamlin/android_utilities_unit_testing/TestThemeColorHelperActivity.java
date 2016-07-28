332d9d7c5cef8ad00a6611253dfa43c8bdb9e1de
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-07-28 22:13:59 +0930

    Updated TestThemeColorHelperActivity
    Small Javadoc change

--- unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestThemeColorHelperActivity.java
+++ unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestThemeColorHelperActivity.java
@@ -20,7 +20,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * An Activity which for use in {@link TestThemeColorHelperActivity}.
+ * An Activity for use in {@link TestThemeColorHelperActivity}.
  */
 public final class TestThemeColorHelperActivity extends AppCompatActivity {
 	@Override
