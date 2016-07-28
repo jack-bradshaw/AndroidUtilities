75bde9f9d030e566ba00adbc1c2492e13334deaf
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-07-28 23:37:02 +0930

    Updated TestActivity.java
    Removed unnecessary imports

--- unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestActivity.java
+++ unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestActivity.java
@@ -2,7 +2,6 @@ package com.matthewtamlin.android_utilities_unit_testing;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.LinearLayout;
 
 /**
