0b9075eb3078f60951d4b4e2632334983d55f018
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-07-28 22:42:13 +0930

    Updated TestActivity.java
    Changed return type of rootView to LinearLayout

--- unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestActivity.java
+++ unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestActivity.java
@@ -3,6 +3,7 @@ package com.matthewtamlin.android_utilities_unit_testing;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * An Activity for use in instrumented unit testing.
@@ -18,7 +19,7 @@ public class TestActivity extends AppCompatActivity {
 	/**
 	 * @return the root view of the activity, null if {@link #onCreate(Bundle)} has not yet run
 	 */
-	public View getRootView() {
-		return findViewById(R.id.activity_test_root);
+	public LinearLayout getRootView() {
+		return (LinearLayout) findViewById(R.id.activity_test_root);
 	}
 }
