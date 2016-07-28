ba5e736ca39fc1bf99a5db8de7e47472a1a21b3c
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-07-28 22:28:14 +0930

    Added layout to TestActivity
    - Created layout file
    - Assigned layout to activity

--- unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestActivity.java
+++ unit testing/src/main/java/com/matthewtamlin/android_utilities_unit_testing/TestActivity.java
@@ -3,10 +3,14 @@ package com.matthewtamlin.android_utilities_unit_testing;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * An Activity for use in instrumented unit testing.
+ */
 public class TestActivity extends AppCompatActivity {
 	@Override
 	protected final void onCreate(final Bundle savedInstanceState) {
 		setTheme(R.style.TestTheme);
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
 	}
 }
