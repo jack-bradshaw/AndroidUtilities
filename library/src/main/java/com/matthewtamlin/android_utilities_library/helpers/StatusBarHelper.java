9f74180db221955367a6322533f41474beadd47d
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-08-07 14:07:11 +0930

    Updated StatusBarHelper
    - Significant changes to public API
    - Javadoc changes
    - Refactoring
    - Class now required API 16 to function

--- library/src/main/java/com/matthewtamlin/android_utilities_library/helpers/StatusBarHelper.java
+++ library/src/main/java/com/matthewtamlin/android_utilities_library/helpers/StatusBarHelper.java
@@ -16,15 +16,16 @@
 
 package com.matthewtamlin.android_utilities_library.helpers;
 
-import android.graphics.Color;
-import android.os.Build;
+import android.annotation.TargetApi;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 
 /**
  * Utilities for hiding and showing the status bar.
  */
+@RequiresApi(16) // For client
+@TargetApi(16) // For lint
 public abstract class StatusBarHelper {
 	/**
 	 * Flag for showing the status bar.
@@ -38,50 +39,31 @@ public abstract class StatusBarHelper {
 			View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
 
 	/**
-	 * Hides the status bar and allows views to draw behind it. If the API level is 21 or higher,
-	 * the status bar will become entirely transparent. If the API level is 19 or 20, the status bar
-	 * will become tinted. Note that views must still specify {@code android:fitsSystemWindows="false"}
-	 * to draw under the status bar.
+	 * Hides the status bar of the supplied Window.
 	 *
 	 * @param window
 	 * 		the Window containing the status bar to hide
 	 */
 	public static void hideStatusBar(final Window window) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			window.setStatusBarColor(Color.TRANSPARENT);
-		} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+		if (window == null) {
+			throw new IllegalArgumentException("window cannot be null");
 		}
 
-		window.getDecorView().setSystemUiVisibility(UI_VISIBILITY_FLAG_HIDE);
+		final View decorView = window.getDecorView();
+		decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 	}
 
 	/**
-	 * Shows the status bar and prevents view from drawing behind it. The color of the status bar
-	 * can be set if the API level is greater than 20.
+	 * Shows the status bar of the supplied Window.
 	 *
 	 * @param window
-	 * 		the Window containing the status bar to hide
-	 * @param statusBarColor
-	 * 		the color to make the status bar (has no effect if API level is less than 21)
+	 * 		the Window containing the status bar to hide, not null
 	 */
-	public static void showStatusBar(final Window window, int statusBarColor) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			window.setStatusBarColor(statusBarColor);
-		} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+	public static void showStatusBar(final Window window) {
+		if (window == null) {
+			throw new IllegalArgumentException("window cannot be null");
 		}
 
 		window.getDecorView().setSystemUiVisibility(UI_VISIBILITY_FLAG_SHOW);
-	}
-
-	/**
-	 * Shows the status bar and prevents view from drawing behind it. The status bar will be black.
-	 *
-	 * @param window
-	 * 		the Window containing the status bar to hide
-	 */
-	public static void showStatusBar(final Window window) {
-		showStatusBar(window, Color.BLACK);
 	}
 }
