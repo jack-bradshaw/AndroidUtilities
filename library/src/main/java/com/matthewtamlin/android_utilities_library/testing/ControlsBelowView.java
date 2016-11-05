611acdcaef0ccbac4e897760b1807b6985479657
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-11-05 19:20:55 +1030

    Created ControlsBelowView.java
    Created class

---
+++ library/src/main/java/com/matthewtamlin/android_utilities_library/testing/ControlsBelowView.java
@@ -0,0 +1,70 @@
+package com.matthewtamlin.android_utilities_library.testing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.matthewtamlin.android_utilities_library.R;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+/**
+ * A TestHarness which displays control buttons below the test view.
+ *
+ * @param <V>
+ * 		the type of view being tested
+ */
+public abstract class ControlsBelowView<V> extends TestHarness<V, FrameLayout> {
+	@Override
+	protected void onCreate(final @Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.controlsbelowview);
+		addTestViewToLayout();
+		initialiseControlHiding();
+	}
+
+	@Override
+	public View getRootView() {
+		return findViewById(R.id.controlsBelowView_root);
+	}
+
+	@Override
+	public LinearLayout getControlButtonContainer() {
+		return (LinearLayout) findViewById(R.id.controlsBelowView_controlsContainer);
+	}
+
+	@Override
+	public FrameLayout getTestViewContainer() {
+		return (FrameLayout) findViewById(R.id.controlsBelowView_testViewContainer);
+	}
+
+	/**
+	 * Adds the test view to the layout. This method will fail if the test view is not an instance
+	 * of the {@link View} class.
+	 */
+	private void addTestViewToLayout() {
+		getTestViewContainer().addView((View) getTestView());
+	}
+
+	/**
+	 * Configures a button to hide/show the controls when clicked.
+	 */
+	private void initialiseControlHiding() {
+		final Button toggleControlVisibilityButton = (Button) findViewById(R.id
+				.controlsBelowView_hideShowControlsButton);
+		final LinearLayout controlButtonContainer = (LinearLayout) findViewById(R.id
+				.controlsBelowView_controlsContainer);
+
+		toggleControlVisibilityButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				controlButtonContainer.setVisibility(controlButtonContainer.getVisibility() ==
+						VISIBLE ? GONE : VISIBLE);
+			}
+		});
+	}
+}
