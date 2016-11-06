bdea427be6ccd5b5f0102bf93af6c5a7b7c3a3a2
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-11-06 12:23:52 +1030

    Updated ColorHelperTestHarness.java
    - Added Javadoc
    - Updated imports

--- testing/src/main/java/com/matthewtamlin/android_utilities/testing/ColorHelperTestHarness.java
+++ testing/src/main/java/com/matthewtamlin/android_utilities/testing/ColorHelperTestHarness.java
@@ -2,12 +2,17 @@ package com.matthewtamlin.android_utilities.testing;
 
 import android.os.Bundle;
 
+import com.matthewtamlin.android_utilities.library.helpers.ColorHelper;
 import com.matthewtamlin.android_utilities.library.testing.NoViewTestHarness;
 
+/**
+ * A test harness for testing the {@link ColorHelper} class. The TestTheme is applied to
+ * facilitate testing.
+ */
 public class ColorHelperTestHarness extends NoViewTestHarness {
 	@Override
 	protected final void onCreate(final Bundle savedInstanceState) {
-		setTheme(com.matthewtamlin.android_utilities.testing.R.style.TestTheme);
+		setTheme(R.style.TestTheme);
 		super.onCreate(savedInstanceState);
 	}
 }
