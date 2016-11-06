c2409582f3953fab4ba854fcc3091a624c49669a
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-11-06 12:52:23 +1030

    Renamed ColorHelperTestHarness.java
    - Renamed class to ThemeColorHelperTestHarness
    - Updated references

--- testing/src/main/java/com/matthewtamlin/android_utilities/testing/ThemeColorHelperTestHarness.java
+++ testing/src/main/java/com/matthewtamlin/android_utilities/testing/ThemeColorHelperTestHarness.java
@@ -9,7 +9,7 @@ import com.matthewtamlin.android_utilities.library.testing.NoViewTestHarness;
  * A test harness for testing the {@link ColorHelper} class. The TestTheme is applied to
  * facilitate testing.
  */
-public class ColorHelperTestHarness extends NoViewTestHarness {
+public class ThemeColorHelperTestHarness extends NoViewTestHarness {
 	@Override
 	protected final void onCreate(final Bundle savedInstanceState) {
 		setTheme(R.style.TestTheme);
