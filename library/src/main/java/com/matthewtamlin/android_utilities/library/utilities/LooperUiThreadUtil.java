2b34106d8ba4f456530b8b5ff71a7ade37a2fa46
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-11-13 18:31:38 +1030

    Updated LooperUiThread
    - Made the to use an instance variable
    - Added constructor
    - Added static factory methods
    - Updated Javadoc

--- library/src/main/java/com/matthewtamlin/android_utilities/library/utilities/LooperUiThreadUtil.java
+++ library/src/main/java/com/matthewtamlin/android_utilities/library/utilities/LooperUiThreadUtil.java
@@ -6,9 +6,43 @@ import android.os.Looper;
 import com.matthewtamlin.android_utilities.library.checkers.NullChecker;
 
 /**
- * Provides access to the UI thread using the main looper.
+ * Provides access to the UI thread using a Looper.
  */
 public class LooperUiThreadUtil implements UiThreadUtil {
+	private final Looper looper;
+
+	/**
+	 * Constructs a new LooperUiThreadUtil which uses the supplied looper.
+	 *
+	 * @param looper
+	 * 		the looper to use, not null
+	 * @return the new LooperUiThreadUtil, not null
+	 * @throws IllegalArgumentException
+	 * 		if {@code looper} is null
+	 */
+	public static LooperUiThreadUtil createUsingLooper(final Looper looper) {
+		return new LooperUiThreadUtil(looper);
+	}
+
+	/**
+	 * Constructs a new LooperUiThreadUtil which uses the main looper of the calling application.
+	 *
+	 * @return the new LooperUiThreadUtil, not null
+	 */
+	public static LooperUiThreadUtil createUsingMainLooper() {
+		return new LooperUiThreadUtil(Looper.getMainLooper());
+	}
+
+	/**
+	 * Constructs a new LooperUiThreadUtil.
+	 *
+	 * @param looper
+	 * 		the looper to use, not null
+	 */
+	public LooperUiThreadUtil(final Looper looper) {
+		this.looper = NullChecker.checkNonNull(looper, "looper cannot be null");
+	}
+
 	@Override
 	public void runOnUiThread(final Runnable runnable) {
 		if (runnable != null) {
