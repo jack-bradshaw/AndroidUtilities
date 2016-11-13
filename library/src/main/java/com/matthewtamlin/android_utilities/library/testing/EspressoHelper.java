5e7f7ab98d8a53a030741f31537106cdd72d0179
Matthew Tamlin <m.d.tamlin@gmail.com>
2016-11-14 08:01:34 +1030

    Renamed EspressoUtil
    Renamed to EspressoHelper

--- library/src/main/java/com/matthewtamlin/android_utilities/library/testing/EspressoHelper.java
+++ library/src/main/java/com/matthewtamlin/android_utilities/library/testing/EspressoHelper.java
@@ -9,7 +9,7 @@ import com.matthewtamlin.android_utilities.library.R;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.withTagKey;
 
-public class EspressoUtil {
+public class EspressoHelper {
 	public static ViewInteraction viewToViewInteraction(final View view) {
 		view.setTag(R.id.espresso_util_conversion_tag, "test");
 		return onView(withTagKey(R.id.espresso_util_conversion_tag));
