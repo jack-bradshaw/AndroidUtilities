32514b86b094183763394ccb36e46e31cb4bc082
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-18 17:25:34 +1030

    Removed unused imports

--- library/src/main/java/com/matthewtamlin/android_utilities/library/helpers/BitmapEfficiencyHelper.java
+++ library/src/main/java/com/matthewtamlin/android_utilities/library/helpers/BitmapEfficiencyHelper.java
@@ -16,7 +16,6 @@
 
 package com.matthewtamlin.android_utilities.library.helpers;
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
