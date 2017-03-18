0b759d10ebadc72fc681118ea938115e73fa6f8a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-18 20:22:11 +1030

    Created InternetHelper.java

---
+++ library/src/main/java/com/matthewtamlin/android_utilities/library/helpers/InternetHelper.java
@@ -0,0 +1,12 @@
+package com.matthewtamlin.android_utilities.library.helpers;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Provides information about the current internet connection.
+ */
+public class InternetHelper {
+	
+}
