cf1791edb8a8f91ec87b847979b41ad13d8c7e4b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-18 20:22:34 +1030

    Added method connectedToInternet

--- library/src/main/java/com/matthewtamlin/android_utilities/library/helpers/InternetHelper.java
+++ library/src/main/java/com/matthewtamlin/android_utilities/library/helpers/InternetHelper.java
@@ -8,5 +8,16 @@ import android.net.NetworkInfo;
  * Provides information about the current internet connection.
  */
 public class InternetHelper {
-	
+	public static boolean connectedToInternet(final Context context) {
+		final ConnectivityManager connectivityManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		final NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+		if (activeNetworkInfo == null) {
+			return false;
+		} else {
+			return activeNetworkInfo.isConnected();
+		}
+	}
 }
